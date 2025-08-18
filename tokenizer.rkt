2d9b0a4fe3198730d1e45375a52ce89c981e2a12
James Geddes <jgeddes@turing.ac.uk>
2025-08-18 15:56:53 +0100

    Working on tokenizer

--- tokenizer.rkt
+++ tokenizer.rkt
@@ -1,5 +1,9 @@
 #lang racket/base
 
+(require json
+         racket/port
+         racket/dict)
+
 ;; Basic Llama 2-type tokenizer
 ;;
 ;; The general approach is described in, for example:
@@ -20,17 +24,77 @@
 
 (struct tokeniser (vocab merges) #:transparent)
 
+;; read-tokeniser/json : port? -> tokeniser?
+;; Read a tokeniser represented as JSON. The JSON must be of the form:
+;;
+;; { ...,
+;;   "model" : { "vocab" : <dictionary>, 
+;;               "merges": <array>
+;;             }  
+;; }
+;;
+;; All fields in "...", except those shown, are ignored. <dictionary>
+;; is a map from strings to numbers; <array> is an array of strings,
+;; where each string is two parts separated by a space.
+;; 
+(define (read-tokeniser/json in)
+  (define (string->merge-rule r vocab)
+    (void))
+  
+  (let* ([tokeniser/json (read-json in)]
+         [model          (dict-ref tokeniser/json 'model)])
+    (let* ([vocab/jsexpr  (dict-ref model 'vocab)]    ; a hash?  
+           [merges/jsexpr (dict-ref model 'merges)]) ; a list?
+      ;; Convert symbol-valued keys to strings,
+      ;; parse the merge rules, and replace strings in the
+      ;; merge rules with the token ids
+      (let ([vocab (hash-map/copy (λ (k v) (values (symbol->string k) v)) vocab)])
+        
+        ))
+    (tokeniser
+     
+     (map (λ (m) (string->merge-rule m)) merges)
+     ) (displayln (dict-count vocab))
+    (displayln (length merges)))
+  )
 
 
 
 (module+ test
-  (define *tkzr* #<<EOF
 
-{ model: 
-  { vocab: 
-    }}
+  ;; picked pickled pickles, from the reference above
+  (define *tkzr* #<<EOF
+{
+    "model": {
+	"vocab": {
+	    "▁" : 0,
+	    "p" : 1,
+	    "i" : 2,
+	    "c" : 3,
+	    "k" : 4,
+	    "l" : 5,
+	    "e" : 6,
+	    "d" : 7,
+	    "s" : 8,
+	    "pi" : 9,
+	    "ck" : 10,
+	    "ed" : 11,
+	    "pick" : 12,
+	    "pickl" : 13
+	},
+	"merges" : [
+	    "p i",
+	    "c k",
+	    "e d",
+	    "pi ck",
+	    "pick l"
+	]
+    }
+}
 EOF
-)
+    )
+
+
 
   (println *tkzr*)
   
